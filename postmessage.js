b2483ba74e6bcaa58fd58797b825ab3b08592ce4
Chris V <50410061+cqvo@users.noreply.github.com>
2024-03-02 10:42:31 -0500

    Update postmessage.js

--- postmessage.js
+++ postmessage.js
@@ -1,15 +1,17 @@
-window.mParticle.ready(
-    function () {
-        console.log('INNER FRAME - mParticle ready!');
-        let classyDeviceId = window.mParticle.getDeviceId();
-        console.log('INNER FRAME - classyDeviceId: ' + classyDeviceId);
-        try {
-            window.parent.postMessage({
-                classyDeviceId: classyDeviceId,
-            }, 'https://github.io');
-            console.log('INNER FRAME - postMessage() sent');
-        } catch (e) {
-            console.log(error);
+function sendDeviceId() {
+    window.mParticle.ready(
+        function () {
+            console.log('INNER FRAME - mParticle ready!');
+            let classyDeviceId = window.mParticle.getDeviceId();
+            console.log('INNER FRAME - classyDeviceId: ' + classyDeviceId);
+            try {
+                window.parent.postMessage({
+                    classyDeviceId: classyDeviceId,
+                }, 'https://github.io');
+                console.log('INNER FRAME - postMessage() sent');
+            } catch (e) {
+                console.log(error);
+            }
         }
-    }
-);
+    );
+}
