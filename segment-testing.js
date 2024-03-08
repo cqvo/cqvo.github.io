4b3d540b81b213785a4e91cf063c42b77e34ba8a
Chris V <50410061+cqvo@users.noreply.github.com>
2024-03-07 19:55:01 -0500

    update

--- segment-testing.js
+++ segment-testing.js
@@ -1,6 +1,24 @@
-document.querySelector('#identify-api')
+window.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.api-button');
+    buttons.forEach(element => {
+        element.addEventListener(
+            'click', 
+            function () {
+                if (typeof window[element.id] === 'function') {
+                    window[element.id]();
+                } else {
+                    console.error('No function found for: ' + element.id);
+                }
+            }
+        );
+    })
+})
 
-let buttons = document.querySelectorAll('.api-button');
-buttons.forEach(element => {
-    
-});
+function track_api() {
+    let eventName = getValues('event_name');
+    analytics.track(eventName);
+}
+
+function getValues(element) {
+    let elementData = document.getElementById(element).value;
+}
